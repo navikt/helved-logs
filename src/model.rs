b16065ea3f0070045ee191ceaa695876f1755cb4
robin <robin.tordly@gmail.com>
2025-10-03 13:06:23 +0200

    resolver urls i egne funksjoner

--- src/model.rs
+++ src/model.rs
@@ -22,34 +22,10 @@ impl Log {
 
     pub fn to_slack_alert(&self, container: String, pod: String) -> serde_json::Value {
         let cluster = crate::env("NAIS_CLUSTER_NAME");
-        let datasource = match cluster.as_str() {
-            "dev-gcp" => "22P95CC91DC09CABFC8", 
-            "prod-gcp" => "22P8A28344D07741F8D", 
-            _ => {
-                println!("could not resolve cluster name, default to dev");
-                "22P95CC91DC09CABFC8"
-            }
-        };
         let trace_id = self.trace_id.clone().unwrap_or("".into());
-        let grafana_trace_url = format!("https://grafana.nav.cloud.nais.io/explore?schemaVersion=1&panes=%7B%22trace%22%3A%7B%22datasource%22%3A%{datasource}%22%2C%22queries%22%3A%5B%7B%22queryType%22%3A%22traceql%22%2C%22query%22%3A%22{trace_id}%22%7D%5D%7D%7D");
-        let peisen_url = match cluster.as_str() {
-            "dev-gcp" => "https://peisen.intern.dev.nav.no/kafka",
-            "prod-gcp" => "https://peisen.intern.nav.no/kafka",
-            _ => {
-                println!("could not resolve cluster name, default to dev");
-                "https://peisen.intern.dev.nav.no/kafka"
-            },
-        };
-
-        let timestamp = "2025-10-02T19:12:45.229595Z";
-        let team_logs_url = match cluster.as_str() {
-            "dev-gcp"  => format!("https://console.cloud.google.com/logs/query;query=resource.type%3D%22k8s_container%22%0Aresource.labels.cluster_name%3D%22prod%22%0Aresource.labels.container_name%3D%22{container}%22%0Aresource.labels.namespace_name%3D%22helved%22%0Aseverity%3E%3DERROR;cursorTimestamp={timestamp};duration=PT1H?project=helved-dev-9e3f"),
-            "prod-gcp" => format!("https://console.cloud.google.com/logs/query;query=resource.type%3D%22k8s_container%22%0Aresource.labels.cluster_name%3D%22prod%22%0Aresource.labels.container_name%3D%22{container}%22%0Aresource.labels.namespace_name%3D%22helved%22%0Aseverity%3E%3DERROR;cursorTimestamp={timestamp};duration=PT1H?project=helved-prod-119e"),
-            _ => {
-                println!("could not resolve cluster name, default to dev");
-                "https://console.cloud.google.com/logs/query;query=resource.type%3D%22k8s_container%22%0Aresource.labels.cluster_name%3D%22prod%22%0Aresource.labels.container_name%3D%22{container}%22%0Aresource.labels.namespace_name%3D%22helved%22%0Aseverity%3E%3DERROR;cursorTimestamp={timestamp};duration=PT1H?project=helved-dev-9e3f".into()
-            },
-        };
+        let grafana_trace_url = self.resolve_grafana_url(&cluster, &trace_id);
+        let peisen_url = self.resolve_peisen_url(&cluster, &trace_id);
+        let team_logs_url = self.resolve_team_logs_url(&cluster, &container);
 
         json!({
             "blocks": [
@@ -181,4 +157,37 @@ impl Log {
             ]
         })
     }
+
+    fn resolve_team_logs_url(&self, cluster: &str, container: &str) -> String {
+        let host = "https://console.cloud.google.com";
+        let timestamp = self.timestamp.clone().unwrap_or("2025-10-03T09:48:17.739954036Z".into());
+        let namespace = crate::env("NAIS_NAMESPACE");
+        let severity = "ERROR";
+        let query = format!("resource.type%3D%22k8s_container%22%0Aresource.labels.container_name%3D%22{container}%22%0Aresource.labels.namespace_name%3D%22{namespace}%22%0Aseverity%3E%3D{severity}");
+        let duration = "PT1H";
+        let project = match cluster {
+            "prod-gcp" => "helved-prod-119e",
+            _ => "helved-dev-9e3f",
+        };
+
+        format!("{host}/logs/query;query={query};cursorTimestamp={timestamp};duration={duration}?project={project}")
+    }
+
+    fn resolve_grafana_url(&self, cluster: &str, trace_id: &str) -> String {
+        let datasource = match cluster {
+            "prod-gcp" => "22P8A28344D07741F8D", 
+            _ => "22P95CC91DC09CABFC8", 
+        };
+        let host = "https://grafana.nav.cloud.nais.io";
+        format!("{host}/explore?schemaVersion=1&panes=%7B%22trace%22%3A%7B%22datasource%22%3A%{datasource}%22%2C%22queries%22%3A%5B%7B%22queryType%22%3A%22traceql%22%2C%22query%22%3A%22{trace_id}%22%7D%5D%7D%7D")
+    }
+
+    fn resolve_peisen_url(&self, cluster: &str, _: &str) -> String {
+        let host = match cluster {
+            "prod-gcp" => "https://peisen.intern.nav.no/kafka", 
+            _ => "https://peisen.intern.dev.nav.no/kafka", 
+        };
+        host.into()
+    }
 }
+
